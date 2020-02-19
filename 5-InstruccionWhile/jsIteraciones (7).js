f3a74acdfb8bb04cc5cbbd20d99d451c7b66599f
barbosalucas278 <60528589+barbosalucas278@users.noreply.github.com>
2020-02-18 21:56:48 -0300

    Update jsIteraciones (7).js

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,7 +3,7 @@ function mostrar()
 var contador=0;
 var acumulador=0;
 var respuesta='si';
-	while(!(respuesta==="no")){
+	while(!(respuesta=="no")){
 		acumulador += acumulador=parseInt(prompt("Ingrese el número"))
 		contador++
 		respuesta=prompt("¿Desea seguir sumando número?","si o no")
