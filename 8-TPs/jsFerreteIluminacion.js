51e64f3b79f111f725b83ac94f2df27f1799141b
barbosalucas278 <60528589+barbosalucas278@users.noreply.github.com>
2020-02-19 22:01:33 -0300

    ferreteIluminaciòn solucionado

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,46 +8,44 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	var precio = 35;
- 	var cantidad = document.getElementById("Cantidad").value
- 	var marca = document.getElementById("Marca").value
- 	var descuento;
- 	var precioDesc;
- 	var precioFinal;
+function CalcularPrecio() {
+	var precio = 35;
+	var cantidad = parseInt(document.getElementById("Cantidad").value)
+	var marca = document.getElementById("Marca").value
+	var descuento;
+	var desc;
 
- 	switch (cantidad) {
- 		case 1:
- 		case 2: 
- 			precio = 35;
- 			break;
- 		case 3:
- 			if (marca=="ArgentinaLuz") {
- 				descuento = 15;
- 			}else if (marca=="FelipeLamparas") {
- 				descuento = 10;
- 			}else {
- 				descuento = 5;
- 			}break;
- 		case 4:
- 			if (marca=="ArgentinaLuz"|| marca=="FelipeLamparas") {
- 				descuento = 25;
- 			}else {
- 				descuento = 20;
- 			}break;
- 		case 5:
- 			if (marca=="ArgentinaLuz") {
- 				descuento = 40;
- 			}else{
- 				descuento = 30;
- 			}break;
- 		case 6:
- 			descuento = 50;
- 			break;
- 	}
- 	precioDesc = (precio*descuento)/100;
- 	document.getElementById("precioDescuento").value = precioDesc
- 	precioFinal = precioDesc * cantidad;
- 	alert(`El precio final es de: ${precioFinal}`)
- }
+	switch (cantidad) {
+		case 1:
+		case 2:
+			descuento = 0;
+			break;
+		case 3:
+			if (marca == "ArgentinaLuz") {
+				descuento = 15;
+			} else if (marca == "FelipeLamparas") {
+				descuento = 10;
+			} else {
+				descuento = 5;
+			} break;
+		case 4:
+			if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+				descuento = 25;
+			} else {
+				descuento = 20;
+			} break;
+		case 5:
+			if (marca == "ArgentinaLuz") {
+				descuento = 40;
+			} else {
+				descuento = 30;
+			} break;
+		default:
+			descuento = 50;
+			break;
+	}
+	desc = (precio * descuento) / 100;
+	document.getElementById("precioDescuento").value = (precio - desc)
+	precio = (precio + desc) * cantidad;
+	alert(`El precio final es de: ${precio}`)
+}
